f364873e49f953b55bb6cc73487ba5021bc74739
Hardi-n <subhagyachhabra18718@gmail.com>
2025-07-06 13:26:37 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { FaInstagram, FaXTwitter, FaLinkedin } from "react-icons/fa6";
+
+const Footer = () => {
+  return (
+    <footer className="w-full py-4 bg-black-100/55 text-white flex justify-center gap-6">
+      <a
+        href="https://www.instagram.com/itz_subhagya_7/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaInstagram size={24} />
+      </a>
+      <a href="https://x.com/Hardin_02?t=k4Wufc3N7REwo4YWdGvjjA&s=08" target="_blank" rel="noopener noreferrer">
+        <FaXTwitter size={24} />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/subhagya-chhabra-2b89b8222/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaLinkedin size={24} />
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
